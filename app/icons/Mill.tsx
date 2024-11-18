bb1bab7bbceb83b446104f6aa9715f094506990a
sam-c14 <samuelaffah14@gmail.com>
2024-11-18 21:42:05 +0100

    Move icon seperate component

---
+++ app/icons/Mill.tsx
@@ -0,0 +1,20 @@
+const Mill = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-5 h-5 mr-2"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M5.25 3.75L12 10.5l6.75-6.75M12 10.5v11.25"
+      />
+    </svg>
+  );
+};
+
+export default Mill;
