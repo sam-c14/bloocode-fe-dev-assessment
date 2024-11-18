10a928ffaf7a5a0e19b1c5eabf3618f25344be49
sam-c14 <samuelaffah14@gmail.com>
2024-11-18 19:23:44 +0100

    Update default configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["image.tmdb.org"], // Add the hostname you want to allow
+  },
 };
 
 export default nextConfig;
