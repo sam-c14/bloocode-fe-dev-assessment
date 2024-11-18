a2a1816fc4a8c30f99f2082400991e4a6231de0f
sam-c14 <samuelaffah14@gmail.com>
2024-11-18 19:20:26 +0100

    Add module to manage typing of app

---
+++ app/types/index.ts
@@ -0,0 +1,68 @@
+export type Movie = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+export interface PopularMovies {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export type CastMember = {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string | null;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  order: number;
+};
+
+export type MovieDetails = Movie & {
+  belongs_to_collection?: {
+    id: number;
+    name: string;
+    poster_path: string | null;
+    backdrop_path: string | null;
+  };
+  budget?: number;
+  genres?: { id: number; name: string }[];
+  homepage?: string;
+  imdb_id?: string;
+  origin_country?: string[];
+  production_companies?: {
+    id: number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries?: { iso_3166_1: string; name: string }[];
+  revenue?: number;
+  runtime?: number;
+  spoken_languages?: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+  status?: string;
+  tagline?: string;
+};
